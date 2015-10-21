db5349e69f906607e16005a82ecd367e15020e8b
Bruce Fenske <bwfenske@ualberta.ca>
2015-10-21 16:33:15 -0600

    * Started DynamicProxy class to load models with ColumnProxies to store values

--- py_linq/queryable/entity/proxy.py
+++ py_linq/queryable/entity/proxy.py
@@ -1,7 +1,9 @@
 __author__ = 'Bruce.Fenske'
 
+import inspect
 from .column_types import Column
 from ...exceptions import InvalidArgumentError
+
 
 class ColumnProxy(object):
     def __init__(self, column, value):
@@ -20,4 +22,38 @@ class ColumnProxy(object):
 
     @value.setter
     def value(self, value):
+        if type(value) != self.column.column_type:
+            raise InvalidArgumentError(u"{0} does not equal {1}".format(value, self.column.column_type))
         self._value = value
+
+
+class DynamicModelProxy(object):
+    def __init__(self, model):
+        """
+        Instantiates a proxy class of a given model. This proxy class auto-adds attributes from the model and
+        instantiates a column proxy object for each attribute. The value for each attribute is set as well.
+        :param model: A child class of a py_linq.queryable.entity.model.Model
+        :return:
+        """
+        self._model = model
+        self.__add_attributes()
+
+    @property
+    def model(self):
+        return self._model
+
+    def __add_attributes(self):
+        attrs = {}
+        columns = [
+            (unicode(name), col) for name, col in inspect.getmembers(self.model)
+            if isinstance(col, Column)
+        ]
+        for name, col in columns:
+            attrs.setdefault(name, ColumnProxy(col, None))
+        self.__dict__.update(attrs)
+        self.__make_properties(attrs)
+
+    def __make_properties(self, attrs):
+        for k,v in attrs:
+            raise NotImplementedError()
+
